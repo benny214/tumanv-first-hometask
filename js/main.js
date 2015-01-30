d7eeeb3f6b8fcee8143e1525b6d33dfb791289ee
Benny214 <benny.moralez@gmail.com>
2015-01-30 10:42:01 +0300

    Исправлены замечания

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+//открываем можаль по клику
+$('.open_popup').click(function() {
+	var popup_id = $('#' + $(this).attr("rel"));
+	$(popup_id).show();$('.overlay').show();
+});
 
+//закрываем модаль по клику 
+$('.popup .close, .overlay').click(function() {
+	$('.overlay, .popup').hide();
+});
